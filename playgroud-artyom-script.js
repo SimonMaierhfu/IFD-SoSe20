d5b7339090525af50556a6616d038d1aaf55fbb7
SimonMaierhfu <72860982+SimonMaierhfu@users.noreply.github.com>
2020-12-02 16:08:54 +0100

    Update playgroud-artyom-script.js

--- playgroud-artyom-script.js
+++ playgroud-artyom-script.js
@@ -58,8 +58,7 @@ artyom.when("NOT_COMMAND_MATCHED", function(){
                     debug: true
                 }).then(function () {
                   
-                 artyom.say("Hi ich bin der Sprachassistent Eve. Ich versuche dir deinen Uni Altag zu erleichtern. Du kannst mich zum Beispiel nach aktuellen Infos fragen.",{
-                 });
+                 artyom.say("Hi ich bin der Sprachassistent Marie. Ich versuche dir deinen Uni Altag zu erleichtern. Du kannst mich zum Beispiel nach aktuellen Infos fragen.");
                  document.getElementById("text").innerHtml= 'Du kannst folgende Befehle testen <br><br> "Gib mir eine Zusammenfassung?" <br> "Was für Veranstaltungen habe ich heute" <br> "In welchem Raum findet Medienethik statt?" <br> "Sag mir die aktuellen Infos."<br>';
                 });
             
